d7eee49afcbb864c3846275ccced5261a75fffe9
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2022-02-01 17:20:51 +0530

    excelSheetColumnTitle solved

---
+++ 2. Amazon/excelSheetColumnTitle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res = "";
+        while(columnNumber){
+            columnNumber--;
+            
+            res += 'A'+(columnNumber%26);
+            
+            columnNumber /=  26;
+            
+        }
+        
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
